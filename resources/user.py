45e5bb209e658958be243098fcaf16d033e47081
Amanda Pearce <amandagpearce@gmail.com>
2023-09-18 07:26:21 -0300

    refresh token route

--- resources/user.py
+++ resources/user.py
@@ -89,3 +89,10 @@ class UserLogout(MethodView):
         return {"message": "Logout executado."}, 200
 
 
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {"access_token": new_token}
