df1dff90c881df86a3aff02cea338f50de085771
Amanda Pearce <amandagpearce@gmail.com>
2023-09-18 07:23:20 -0300

    app.py configs

--- app.py
+++ app.py
@@ -1,3 +1,42 @@
+import os
+from dotenv import load_dotenv
+from flask_cors import CORS
+from flask import Flask, jsonify
+from flask_smorest import Api
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+
+from resources.user import blp as UserBlueprint
+from models.token_blocklist import TokenBlocklistModel
+from db import db
+
+
+def create_app(db_url=None):  # factory pattern
+    app = Flask(__name__)
+    CORS(app)
+
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Authentication REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/doc"
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL",
+        "sqlite:///data.db",  # if database_url is not found, default to sqlite
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)  # initializes sqlalchemy
+    migrate = Migrate(app, db)  # noqa
+    api = Api(app)
+
+    load_dotenv()
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+    jwt = JWTManager(app)
+
     @jwt.token_in_blocklist_loader
     def check_if_token_in_blocklist(jwt_header, jwt_payload):
         tokenExists = TokenBlocklistModel.query.filter(
@@ -56,3 +95,10 @@
             },
             401,
         )
+
+    with app.app_context():
+        db.create_all()  # creating the dbs if they dont already exist
+
+    api.register_blueprint(UserBlueprint)
+
+    return app
